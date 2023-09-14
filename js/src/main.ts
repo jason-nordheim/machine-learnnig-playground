d5dcb2506e6c19b79ff9317f130d1959cb7ba5a6
Jason Nordheim <jason.nordheim@gmail.com>
2023-09-14 14:29:55 -0600

    implemented app functionality

--- js/src/main.ts
+++ js/src/main.ts
@@ -22,9 +22,10 @@ const drawPaths = (ctx: CanvasRenderingContext2D, paths: number[][][], color = "
 };
 
 class SketchPad {
+  private undoBtn: HTMLButtonElement;
   private canvas: HTMLCanvasElement;
-  private paths: number[][][];
-  private isDrawing: boolean;
+  private paths: number[][][] = [];
+  private isDrawing: boolean = false;
 
   constructor(container: Element, size = 400) {
     this.canvas = document.createElement("canvas");
@@ -34,10 +35,16 @@ class SketchPad {
     this.canvas.style.boxShadow = `0px 0px 10px 2px black`;
     container.appendChild(this.canvas);
 
+    const controls = document.createElement("div");
+    container.appendChild(controls);
+
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "Undo";
+    container.appendChild(this.undoBtn);
+
     this.ctx.scale(1, 1);
 
-    this.paths = [];
-    this.isDrawing = false;
+    this.reset();
 
     this.addEventListeners();
   }
@@ -49,6 +56,11 @@ class SketchPad {
   private reDraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.width);
     drawPaths(this.ctx, this.paths);
+    if (this.paths.length > 0) {
+      this.undoBtn.disabled = false;
+    } else {
+      this.undoBtn.disabled = true;
+    }
   }
 
   getPosition(evt: MouseEvent | TouchEvent): [number, number] {
@@ -93,23 +105,156 @@ class SketchPad {
     };
   }
 
+  private makeUndoEventHandler() {
+    return () => {
+      this.paths.pop();
+      this.reDraw();
+    };
+  }
+
   private addEventListeners() {
     this.canvas.onmousedown = this.makeStartPathEventHandler();
     this.canvas.onmousemove = this.makeDragPathEventHandler();
-    this.canvas.onmouseup = this.makeDragEndEventHandler();
+    document.onmouseup = this.makeDragEndEventHandler();
     this.canvas.ontouchstart = this.makeStartPathEventHandler();
     this.canvas.ontouchmove = this.makeDragPathEventHandler();
-    this.canvas.ontouchend = this.makeDragEndEventHandler();
+    document.ontouchend = this.makeDragEndEventHandler();
+
+    this.undoBtn.onclick = this.makeUndoEventHandler();
+  }
+
+  public reset() {
+    this.paths = [];
+    this.isDrawing = false;
+    this.reDraw();
+  }
+
+  public getPaths() {
+    return this.paths;
   }
 }
 
-const init = () => {
-  const container = document.querySelector("#sketchPadContainer");
-  if (!container) {
-    throw new Error("Cannot find sketch pad container");
-  }
-
-  const sketchPad = new SketchPad(container);
+type SessionData = {
+  name?: string;
+  sessionId?: number;
+  drawings?: { [k: string]: number[][][] };
 };
 
-init();
+class App {
+  private labels: string[] = ["car", "fish", "tree", "bicycle", "guitar", "pencil", "clock"];
+  private root: Element;
+  private instructions: HTMLSpanElement;
+  private sketchPadContainerRef: HTMLDivElement;
+  private inputContainerRef: HTMLDivElement;
+  private inputRef: HTMLInputElement;
+  private nextBtn: HTMLButtonElement;
+  private sketchPad: SketchPad;
+  private index: number = 0;
+
+  private data: SessionData;
+
+  constructor(root: Element) {
+    this.data = {
+      name: "",
+      sessionId: undefined,
+      drawings: {},
+    };
+    this.root = root;
+
+    this.inputContainerRef = document.createElement("div");
+    this.inputContainerRef.style.display = "flex";
+    this.inputContainerRef.style.justifyContent = "center";
+    this.inputContainerRef.style.gap = "5px";
+    this.inputContainerRef.style.padding = "5px";
+
+    this.inputRef = document.createElement("input");
+    this.inputRef.type = "text";
+    this.inputRef.placeholder = "type your name";
+
+    this.nextBtn = document.createElement("button");
+    this.nextBtn.textContent = "Start";
+    this.nextBtn.onclick = this.createStartHandler();
+
+    this.instructions = document.createElement("span");
+    this.inputContainerRef.appendChild(this.instructions);
+
+    this.sketchPadContainerRef = document.createElement("div");
+    this.sketchPadContainerRef.style.visibility = "hidden";
+
+    this.sketchPad = new SketchPad(this.sketchPadContainerRef);
+
+    this.inputContainerRef.appendChild(this.inputRef);
+    this.inputContainerRef.appendChild(this.nextBtn);
+
+    this.root.appendChild(this.inputContainerRef);
+    this.root.appendChild(this.sketchPadContainerRef);
+  }
+
+  private createStartHandler() {
+    return () => {
+      if (this.inputRef.value.length == 0 || this.inputRef.value === "") {
+        alert("Please enter your name");
+        return;
+      }
+
+      this.data.name = this.inputRef.value;
+      this.inputRef.style.display = "none";
+      this.sketchPadContainerRef.style.visibility = "visible";
+      this.instructions.textContent = `Please draw a ${this.labels[this.index]}`;
+      this.nextBtn.textContent = "Next";
+      this.nextBtn.onclick = this.createNextEventHandler();
+    };
+  }
+
+  private createNextEventHandler() {
+    return () => {
+      const paths = this.sketchPad.getPaths();
+      if (paths.length < 1) {
+        alert("Please draw something before proceeding");
+        return;
+      }
+
+      if (!this.data.drawings) {
+        throw new Error("Drawing data not initialized");
+      }
+
+      this.data.drawings[this.labels[this.index]] = paths;
+      this.sketchPad.reset();
+
+      if (this.index < this.labels.length - 1) {
+        this.index++;
+        this.instructions.textContent = `Please draw a ${this.labels[this.index]}`;
+      } else {
+        this.data.sessionId = new Date().getTime();
+        this.sketchPadContainerRef.style.visibility = "hidden";
+        this.instructions.textContent = "Thank you";
+        this.nextBtn.textContent = "Save";
+        this.nextBtn.onclick = this.createSaveEventHandler();
+      }
+    };
+  }
+
+  private createSaveEventHandler() {
+    return () => {
+      this.nextBtn.style.display = "none";
+      this.instructions.textContent = `Take your downloaded file and place it alongside other data`;
+      const fileName = `${this.data.sessionId}.json`;
+
+      const anchor = document.createElement("a");
+      anchor.style.display = "none";
+      anchor.download = fileName;
+      anchor.href = `data:text/plan;charset=utf-9,${encodeURIComponent(JSON.stringify(this.data))}`;
+
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+    };
+  }
+}
+
+const container = document.querySelector("#app");
+if (!container) {
+  throw new Error("Cannot find sketch pad container");
+}
+
+const sketchPad = new App(container);
